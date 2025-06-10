0eb0fd97a0f4c94a362627a131807fea756bcae1
maojude <maojude04@gmail.com>
2025-06-10 13:06:49 +0800

    Create functions for basic math operations

---
+++ backend.js
@@ -0,0 +1,5 @@
+// functions for basic math operations
+const add = (first, second) => first + second;
+const subtract = (first, second) => first - second;
+const multiply = (first, second) => first * second;
+const divide = (first, second) => first / second;
